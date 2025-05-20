bbe946b109845c0d1a310d4c525b90f13a69f0aa
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-05-20 09:52:33 -0300

    Programa iniciarSessao e exigirLogin

--- src/Auth/ControleDeAcesso.php
+++ src/Auth/ControleDeAcesso.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Microblog\Auth;
 
 /* Sobre sessões no PHP 
@@ -7,3 +8,29 @@ Sessão (SESSION) é uma funcionalidade usada principalmente para o controle de 
 Exemplos: áreas administrativas, painel de controle/dashboard, área do cliente, área do aluno etc.
 
 Nestas áreas o acesso se dá através de alguma forma de autenticação. Exemplos: login/senha, biometria, facial, 2-fatores etc. */
+
+final class ControleDeAcesso
+{
+    private function __construct() {}
+
+    /* Inicia uma sessão caso não tenha nenhuma em andamento */
+    private static function iniciarSessao():void
+    {
+        if(!isset($_SESSION)) session_start();
+    }
+
+    /* "Bloqueia" páginas admin caso o usuário NÃO ESTEJA logado */
+    public function exigirLogin(): void
+    {
+        // Inicia sessão (se necessário)
+        self::iniciarSessao();
+        
+        // Se NÃO EXISTIR uma variável de sessão chamada ID
+        if(!isset($_SESSION['id'])){
+            session_destroy();
+            header("location:../login.php?acesso_proibido");
+            exit;
+        }
+    }
+
+}
